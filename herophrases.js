b4866074802d95245cf4530cb8caf8462eb1631f
AppleAccountPerson <282634857+AppleAccountPerson@users.noreply.github.com>
2026-05-08 09:58:05 -0700

    Create herophrases.js

---
+++ herophrases.js
@@ -0,0 +1,18 @@
+const heroPhrases = [
+"Big Chicken Boi",
+"Flock Talk",
+"Nest Fest",
+"No Omelets?",
+"Bassssssssssssssssssssssss",
+"Funny Chicken Pun",
+ "Hi",
+ "subtext",
+ "Funqy",
+ "What's a call and response?",
+ "this looks like that yellow text from that game",
+ "Fr?",
+ "No, you're not crazy",
+ "Chromatic"
+];
+
+export default heroPhrases;
